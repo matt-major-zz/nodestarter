8e7fcb3b4e267dfb92f830bb196dd7c975f7a53f
Matt Major <mattmajor14@gmail.com>
2015-04-05 16:46:54 +0100

    1.1.2 Improved Logging

--- bin/nodestarter.js
+++ bin/nodestarter.js
@@ -10,7 +10,7 @@ var argv        = require('minimist')(process.argv.slice(2)),
     templateDir = argv.templatePath || path.join(__dirname, '..', 'template'),
     destDir     = argv.output || path.join(cwd, argv.name);
 
-function logError(errorMessage, errorCode) {
+function logFailure(errorMessage, errorCode) {
   process.stderr.write(errorMessage + '\n');
   process.exit(errorCode || 1);
 }
@@ -20,16 +20,16 @@ function logInfo(infoMessage) {
 }
 
 if ( !argv.name ) {
-  logError('Project Name required', 3);
+  logFailure('Project Name required. Use --name to provide.', 3);
 }
 if ( !argv.author ) {
-  logError('Author name required', 3);
+  logFailure('Author required. Use --author to provide.', 3);
 }
 if ( !argv.email ) {
-  logError('Email required', 3);
+  logFailure('Email address required. Use --email to provide.', 3);
 }
 if ( !argv.repo ) {
-  logError('Repository required', 3);
+  logFailure('Repository required. Use --repo to provide.', 3);
 }
 
 var data = {
@@ -44,7 +44,7 @@ var data = {
 };
 
 if ( fs.existsSync(destDir) ) {
-  logError('Destination directory already exists', 2);
+  logFailure('Output directory already exists. Please try again.', 2);
 } else {
   fs.mkdirSync(destDir);
 }
@@ -53,8 +53,8 @@ scaffold({
   data: data
 }).copy(templateDir, destDir, function (err) {
   if ( err ) {
-    logError(err, 2);
+    logFailure(err, 2);
   } else {
-    logInfo('Created: "' + data.name + '" ' + data.version);
+    logInfo('Project Created: "' + data.name + '" ' + data.version);
   }
 });
